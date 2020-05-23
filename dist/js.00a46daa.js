36680004ef9ad966e6e99cdba91c43010db3d1bc
richardso21 <richard.so21@sitechhs.com>
2020-05-23 16:30:05 -0400

    Revert "Merge branch 'dev' into message"
    This reverts commit be44c6493154465a5c266eda90d95baec8ba1f26, reversing
    changes made to 031c6f39c310f2975390d0e056a74fc35feb0223.

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -170,6 +170,7 @@ if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "38271" + '/');
+
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- dist/style.a6dae8f7.js
+++ dist/style.a6dae8f7.js
@@ -218,6 +218,7 @@ if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "38271" + '/');
+
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
